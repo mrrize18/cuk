6a391cf2e4aab56d16f342c93ef30c39c1e12f40
mrrize18 <invokerdt2@gmail.com>
2018-12-11 09:46:46 -0300

    bangsat

---
+++ README.md
@@ -0,0 +1,3 @@
+# go-checker
+Multi Account Checker
+# asu
---
+++ token.php
@@ -0,0 +1,6 @@
+<?php 
+
+	// YOUR TOKEN
+	$token = "Minicode_e487589cfb59fe9b4ecc94e249ba634a55fa06c28a33656c5691d32492d373498a3656def6d7df1430ceb265719fc5c684df6b888055aac4903b7338cda7c797_Brilly4n";
+	return $token;
+?>
